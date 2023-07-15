e201e1b05e7bb3620b14e85c21bfae0f06db9593
HimaElSoltan <himaelsoltan1@gmail.com>
2023-07-16 01:09:44 +0300

    final

---
+++ js/js.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+document.getElementById('menu').onclick = function(){
+  this.style.backgroundColor = "black";
+  document.getElementById('navigathion').style.transform = "translateX(100vw)";
+  document.getElementById('head').style.overflow = "hidden";
+}
+
+
+document.getElementById('menu').ondblclick = function(){
+  this.style.backgroundColor = "green";
+  document.getElementById('navigathion').style.transform = "translateX(0)";
+  document.getElementById('head').style.overflow = "visible";
+
+}
+
